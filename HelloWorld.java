207ade934d952cdbab8b2fdd144fb377cf2289cf
Amy Beeston <amybeeston93@gmail.com>
2015-05-11 22:17:10 -0600

    got rid of world

--- HelloWorld.java
+++ HelloWorld.java
@@ -16,7 +16,8 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Github World");
+        System.out.println("Hello Github");
+        System.out.println("Testing...");
     }
     
 }
