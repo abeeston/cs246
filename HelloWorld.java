b3c54331c911d63842d2cffbaedaea1bd531de62
Amy Beeston <amybeeston93@gmail.com>
2015-05-11 22:12:13 -0600

    added testing...

--- HelloWorld.java
+++ HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Github");
+        System.out.println("Hello Github World");
         System.out.println("testing...");
     }
     
